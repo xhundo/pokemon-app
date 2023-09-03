476110871b8541af7f18662e67fe12a546cd3a74
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-03 19:51:49 -0400

    feat: initial

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	/** @type {import('tailwindcss').Config} */
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: ['prettier-plugin-tailwindcss'],
+};
