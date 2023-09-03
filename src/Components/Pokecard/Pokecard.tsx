70b5430112b368f0f54f03f2779c720375af9bed
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-03 19:55:42 -0400

    fix: indentation

--- src/Components/Pokecard/Pokecard.tsx
+++ src/Components/Pokecard/Pokecard.tsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 type skill = {
-	id: number;
-	name: string;
-	type: string;
-	exp: number;
+  id: number;
+  name: string;
+  type: string;
+  exp: number;
 };
 
 class Pokecard extends Component<skill> {
-	render(): React.ReactNode {
-		const { id, name, type, exp } = this.props;
+  render(): React.ReactNode {
+    const { id, name, type, exp } = this.props;
 
-		return (
-			<div className="bg-slate-400 p-3 rounded-xl">
-				<p className="text-blue-500 text-center">{name}</p>
-				<img
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-				/>
-				<p className="text-red-500 text-center">{`type: ${type}`}</p>
-				<p className="text-amber-300 text-center mb-6"> exp: {exp}</p>
-			</div>
-		);
-	}
+    return (
+      <div className="bg-slate-400 p-3 rounded-xl">
+        <p className="text-blue-500 text-center">{name}</p>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        />
+        <p className="text-red-500 text-center">{`type: ${type}`}</p>
+        <p className="text-amber-300 text-center mb-6"> exp: {exp}</p>
+      </div>
+    );
+  }
 }
 
 export { Pokecard };
--- src/Components/Pokedex/Pokedex.tsx
+++ src/Components/Pokedex/Pokedex.tsx
@@ -2,39 +2,39 @@ import React, { Component } from 'react';
 import { Pokecard } from '../Pokecard/Pokecard';
 
 type DexProps = {
-	dex: Array<CharType>;
+  dex: Array<CharType>;
 };
 
 type CharType = {
-	id: number;
-	name: string;
-	type: string;
-	base_experience: number;
+  id: number;
+  name: string;
+  type: string;
+  base_experience: number;
 };
 
 class Pokedex extends Component<DexProps> {
-	render(): React.ReactNode {
-		const { dex } = this.props;
-		return (
-			<div className="flex flex-col justify-center">
-				<h1 className="text-slate-400 text-center text-lg">Pokedex</h1>
-				<div className="flex flex-wrap justify-center">
-					{dex.map((dx_char: CharType) => {
-						return (
-							<li className="list-none">
-								<Pokecard
-									type={dx_char.type}
-									id={dx_char.id}
-									name={dx_char.name}
-									exp={dx_char.base_experience}
-								/>
-							</li>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+  render(): React.ReactNode {
+    const { dex } = this.props;
+    return (
+      <div className="flex flex-col justify-center">
+        <h1 className="text-slate-400 text-center text-lg">Pokedex</h1>
+        <div className="flex flex-wrap justify-center">
+          {dex.map((dx_char: CharType) => {
+            return (
+              <li className="list-none">
+                <Pokecard
+                  type={dx_char.type}
+                  id={dx_char.id}
+                  name={dx_char.name}
+                  exp={dx_char.base_experience}
+                />
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export { Pokedex };
