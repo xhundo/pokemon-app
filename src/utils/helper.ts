d6ba4b13ff95478624db43c51f72c50c19600055
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-04 19:04:55 -0400

    feat: add helper method

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,18 @@
+import type { CharType } from './types';
+
 const padID = (id: number): string => {
   let _id = String(id);
   return _id.length === 1 ? '00' + _id : _id.length === 2 ? '0' + _id : _id;
 };
 
-export { padID };
+const getTotal = (
+  obj: { [dex: string]: Array<CharType> },
+  key: string,
+): number => {
+  return obj[key].reduce(
+    (acc: number, dx_char: CharType) => acc + dx_char.base_experience,
+    0,
+  );
+};
+
+export { padID, getTotal };
